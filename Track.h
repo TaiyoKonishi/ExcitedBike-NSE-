b7b07ae412521ce4ddb02c7b5954b4fd4666d600
TaiyoKonishi <141292080+TaiyoKonishi@users.noreply.github.com>
2024-04-29 07:16:02 +1200

    Update Track.h

--- Track.h
+++ Track.h
@@ -2,14 +2,14 @@
 #define TRACK_H
 
 #include <vector>
-#include "Obstacle.h"
+//#include "Obstacle.h"
 #include <SFML/Graphics.hpp>
-#include "Player.h"
+//#include "Player.h"
 
 // The Track class manages the game environment, including the layout of the track and obstacles.
 class Track {
 public:
-    Track();
+    Track(const std::string& imagePath, float scrollSpeed);
     ~Track();
 
     // Initializes the track by loading its layout and placing obstacles.
@@ -18,7 +18,7 @@ public:
 
     // Updates the state of the track. This includes triggering events related to obstacle movement and player progress.
     // Called from the GameEngine's update loop, checking for player progress and interactions with the track.
-    void Update(float deltaTime, const Player& player);
+    void Update(float deltaTime);
 
     // Renders the track and its obstacles on the screen.
     // Should be called from the GameEngine's rendering loop, providing the rendering context.
@@ -26,20 +26,24 @@ public:
 
     // Checks and handles collisions between the player and obstacles on the track.
     // This function is called from the GameEngine's update loop after updating the player and track positions.
-    void CheckCollisions(Player& player);
+    //void CheckCollisions(Player& player);
 
     // Adds a new obstacle to the track.
-    void AddObstacle(const Obstacle& obstacle);
+    //void AddObstacle(const Obstacle& obstacle);
 
     // Removes a specific obstacle from the track.
-    void RemoveObstacle(const Obstacle& obstacle);
+    //void RemoveObstacle(const Obstacle& obstacle);
 
     // Changes the layout of the track. This can be used, for example, to implement track sections that change as the race progresses.
-    void ChangeLayout();
+    //void ChangeLayout();
 
 private:
-    std::vector<Obstacle> obstacles; // List of obstacles on the track.
+    //std::vector<Obstacle> obstacles; // List of obstacles on the track.
     sf::Sprite trackSprite; // The visual representation of the track.
+    sf::Texture trackTexture;
+    float scrollSpeed;
+    float currentPosition;
+    std::string imagePath;
 };
 
 #endif // TRACK_H
